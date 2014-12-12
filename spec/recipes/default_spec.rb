b712aaf5ac6a5cc8b43bc43e6fccfe6c5344a372
Larry Gilbert <larry@industrialtoys.com>
2014-12-12 14:38:40 -0800

    Add basic ChefSpec tests

---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'ucspi-tcp::default' do
+  let(:chef_log) { class_spy(Chef::Log) }
+
+  context 'on Debian 7.5' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'debian', version: '7.5')
+        .converge(described_recipe)
+    end
+
+    specify { expect(chef_run).to install_package('ucspi-tcp') }
+  end
+
+  context 'on an unknown distro' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'oracle').converge(described_recipe)
+    end
+
+    it 'should log a message to Chef::Log.info' do
+      expect(chef_log).to have_received(:info)
+    end
+  end
+
+  # specify { expect(chef_run).to run_bash('install_ucspi') }
+end
