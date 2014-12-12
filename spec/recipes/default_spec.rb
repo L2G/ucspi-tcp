e6be3584d605af5d31352b68b788848d836c101f
Larry Gilbert <larry@industrialtoys.com>
2014-12-12 15:59:46 -0800

    Add tests for source installation

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 describe 'ucspi-tcp::default' do
   let(:chef_log) { class_spy(Chef::Log) }
 
+  # Shared examples
+
   shared_examples 'a default package installation' do
     specify { expect(chef_run).to install_package('ucspi-tcp') }
     specify { expect(chef_run.node['ucspi']['bin_dir']).to eq('/usr/bin') }
   end
 
   shared_examples 'a default source build and installation' do
-    pending
+    specify { expect(chef_run).to run_bash('install_ucspi') }
+    specify { expect(chef_run.node['ucspi']['bin_dir']).to eq('/usr/local/bin') }
   end
+
+  # Arch
 
   context 'on Arch Linux' do
     let(:chef_run) do
@@ -26,6 +31,32 @@ describe 'ucspi-tcp::default' do
     specify { expect(chef_run.node['ucspi']['bin_dir']).to eq('/usr/bin') }
   end
 
+  # CentOS
+
+  context 'on CentOS 6.5' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5')
+        .converge(described_recipe)
+    end
+
+    it_should_behave_like 'a default source build and installation'
+  end
+
+  # Debian
+
+  # This version is not covered by Fauxhai
+  context 'on Debian 4.0' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['platform'] = 'debian'
+        node.automatic['platform_family'] = 'debian'
+        node.automatic['platform_version'] = '4.0'
+      end.converge(described_recipe)
+    end
+
+    it_should_behave_like 'a default source build and installation'
+  end
+
   context 'on Debian 7.5' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'debian', version: '7.5')
@@ -34,6 +65,8 @@ describe 'ucspi-tcp::default' do
 
     it_should_behave_like 'a default package installation'
   end
+
+  # Gentoo
 
   context 'on Gentoo' do
     let(:chef_run) do
@@ -45,14 +78,35 @@ describe 'ucspi-tcp::default' do
     specify { expect(chef_run.node['ucspi']['bin_dir']).to eq('/usr/bin') }
   end
 
-  context 'on Ubuntu 14.04' do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04')
-        .converge(described_recipe)
-    end
+  # Ubuntu
 
-    it_should_behave_like 'a default package installation'
+  # These versions are not covered by Fauxhai
+  %w(6.06 6.10 7.04 7.10 8.04).each do |ubuntu_version|
+    context "on Ubuntu #{ubuntu_version}" do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new do |node|
+          node.automatic['platform'] = 'ubuntu'
+          node.automatic['platform_family'] = 'debian'
+          node.automatic['platform_version'] = ubuntu_version
+        end.converge(described_recipe)
+      end
+
+      it_should_behave_like 'a default source build and installation'
+    end
   end
+
+  %w(10.04 14.04).each do |ubuntu_version|
+    context "on Ubuntu #{ubuntu_version}" do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new(platform: 'ubuntu', version: ubuntu_version)
+          .converge(described_recipe)
+      end
+
+      it_should_behave_like 'a default package installation'
+    end
+  end
+
+  # Unknown platforms
 
   context 'on an unknown distro' do
     let(:chef_run) do
@@ -63,6 +117,4 @@ describe 'ucspi-tcp::default' do
       expect(chef_log).to have_received(:info)
     end
   end
-
-  # specify { expect(chef_run).to run_bash('install_ucspi') }
 end
